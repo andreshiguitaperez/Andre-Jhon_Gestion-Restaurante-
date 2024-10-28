671a8f72305b58673c8c7cd5fa1f8b92716f7fc7
Juan Pablo <comercial@prevalentware.com>
2024-10-28 06:46:02 -0500

    actualización de branch

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,6 +1,29 @@
 import React, { useEffect } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { GET_USER_BY_ID } from '@/src/utils/gql/queries/users';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/src/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/src/components/ui/form';
+import { Input } from '@/src/components/ui/input';
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  email: z.string().min(2, {
+    message: 'email must be at least 2 characters.',
+  }),
+});
 
 export async function getServerSideProps(context: { params: { id: string } }) {
   const id = context.params.id;
@@ -17,11 +40,58 @@ const Index = ({ id }: { id: string }) => {
     },
   });
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   useEffect(() => {
     getUser({ variables: { userId: id } });
   }, [id]);
 
-  return <div>Hola Mundo Dinamica:{id}</div>;
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+        <FormField
+          control={form.control}
+          name='username'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder='Nombre' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type='email' placeholder='Nombre' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Index;
